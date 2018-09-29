1c8df10ef67ec0f0dda2a1cf2a5fabb7560b1aab
Nelson Crosby <nelsonc@sourcecomb.com>
2018-09-29 12:23:18 +1200

    Initial commit

---
+++ app/src/main/java/com/sourcecomb/ncrosby/mihoclient/SimpleTrackpadActivity.kt
@@ -0,0 +1,12 @@
+package com.sourcecomb.ncrosby.mihoclient
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SimpleTrackpadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simple_trackpad)
+    }
+}
