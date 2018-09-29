c12b187a2a6e60dca2c9b87435655b6128e1e381
Nelson Crosby <nelsonc@sourcecomb.com>
2018-09-29 14:40:37 +1200

    Implement test protocol for mouse move

--- app/src/main/java/com/sourcecomb/ncrosby/mihoclient/RemoteHost.kt
+++ app/src/main/java/com/sourcecomb/ncrosby/mihoclient/RemoteHost.kt
@@ -1,4 +1,46 @@
 package com.sourcecomb.ncrosby.mihoclient
 
-class RemoteHost {
+import android.util.Log
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.doAsyncResult
+import java.net.Socket
+import java.util.concurrent.Future
+
+private val hexArray = "0123456789ABCDEF".toCharArray()
+fun ByteArray.toHex(): String {
+    val hexChars = CharArray(this.size * 2)
+    for (j in this.indices) {
+        val v = this[j].toInt() and 0xFF
+        hexChars[j * 2] = hexArray[v.ushr(4)]
+        hexChars[j * 2 + 1] = hexArray[v and 0x0F]
+    }
+    return String(hexChars)
+}
+
+class RemoteHost(hostname: String) {
+    private lateinit var socket: Socket
+    private var thread: Future<Unit>
+
+    init {
+        thread = doAsync {
+            socket = Socket(hostname, 1234)
+            socket.tcpNoDelay = true
+        }
+    }
+
+    fun sendMouseMove(dx: Int, dy: Int): Future<Unit> {
+        val buf = ByteArray(3)
+        buf[0] = 3
+        buf[1] = dx.toByte()
+        buf[2] = dy.toByte()
+
+        thread = thread.doAsyncResult {
+            Log.d("RemoteHost", "Writing mouse move ${buf.toHex()}")
+            val stream = socket.getOutputStream()
+            stream.write(buf)
+            stream.flush()
+        }
+
+        return thread
+    }
 }
--- app/src/main/java/com/sourcecomb/ncrosby/mihoclient/SimpleTrackpadActivity.kt
+++ app/src/main/java/com/sourcecomb/ncrosby/mihoclient/SimpleTrackpadActivity.kt
@@ -2,16 +2,47 @@ package com.sourcecomb.ncrosby.mihoclient
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 
 class SimpleTrackpadActivity : AppCompatActivity() {
+
+    lateinit var remoteHost: RemoteHost
+
+    private var lastX: Int = 0
+    private var lastY: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simple_trackpad)
 
+        remoteHost = RemoteHost(resources.getString(R.string.default_host))
+
         findViewById<View>(R.id.trackpad).apply {
-            this.setOnDragListener { view, dragEvent -> false }
+            setOnTouchListener { _, motionEvent ->
+                Log.d("SimpleTrackpad", "Trackpad got motionEvent $motionEvent")
+                when (motionEvent.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        Log.d("SimpleTrackpad", "Registered touch start")
+                        lastX = motionEvent.x.toInt()
+                        lastY = motionEvent.y.toInt()
+                        true
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        val newX = motionEvent.x.toInt()
+                        val newY = motionEvent.y.toInt()
+                        val x = (newX - lastX)
+                        val y = (newY - lastY)
+                        lastX = newX
+                        lastY = newY
+                        Log.d("SimpleTrackpad", "Moving mouse ($x, $y)")
+                        remoteHost.sendMouseMove(x, y)
+                        true
+                    }
+                    else -> false
+                }
+            }
         }
     }
 }
